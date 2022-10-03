837d4d09a6f84f926b2c8eb96e21eb7e045a293b
Aidan Tan <aidan.tan@berkeley.edu>
2022-10-03 02:03:36 +0000

    feat(module5): saving on step 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,46 @@
 class MoviesController < ApplicationController
 
 
+    def sort
+      @movies = Movie.all
+    end
+
     def show
+      session[:ratings] ||= params[:ratings]
+      session[:sort_by] ||= params[:sort_by]
       id = params[:id] # retrieve movie ID from URI route
       @movie = Movie.find(id) # look up movie by unique ID
       # will render app/views/movies/show.<extension> by default
     end
   
     def index
+
+      if params[:home] == nil
+        params[:ratings] ||= session[:ratings]
+        params[:sort_by] ||= session[:sort_by]
+      end
+
       if params[:ratings] != nil
         ratings_arr = params[:ratings].keys
         @ratings_to_show = ratings_arr
       end
       @movies = Movie.with_ratings(ratings_arr)
+
+      if params[:sort_by] != nil
+        @movies = @movies.order("#{params[:sort_by]} ASC").all
+      else
+        @movies = @movies.all
+      end
+      
+      if params[:sort_by] == 'title'
+        @title_header = 'bg-warning'
+        @sort_by = 'title'
+      elsif params[:sort_by] == 'release_date'
+        @release_date_header ='bg-warning'
+        @sort_by = 'release_date'
+      end
     end
-      # redirect_to movies_path
+        # redirect_to movies_path
     
     def new
       # default: render 'new' template
@@ -44,7 +70,6 @@ class MoviesController < ApplicationController
       redirect_to movies_path
     end
 
-
     private
     # Making "internal" methods private is not required, but is a common practice.
     # This helps make clear which methods respond to requests, and which ones do not.
@@ -53,10 +78,14 @@ class MoviesController < ApplicationController
     end
 
     before_filter :set_my_variables
+    # before_render :set_session
 
     private
     def set_my_variables
       @all_ratings = Movie.all_ratings
       @ratings_to_show = []
+      @title_header = 'bg-transparent'
+      @release_date_header = 'bg-transparent'
+      @sort_by = nil
     end
   end
